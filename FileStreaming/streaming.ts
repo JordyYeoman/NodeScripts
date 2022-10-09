2bb21f9f1fa3ef828adbcb4d0139d741061620a8
Jordy Yeoman <jordyyeoman@gmail.com>
2022-10-09 13:21:15 +0800

    Adjust upload size

---
+++ FileStreaming/streaming.ts
@@ -0,0 +1,31 @@
+var fs = require("fs");
+
+var data = [""];
+var loop = 0;
+
+var readStream = fs.createReadStream("./IRONHEART_BETA.txt", {
+  highWaterMark: 50 * 1024, // Gives around 1k data points per array
+  encoding: "utf8",
+});
+
+readStream
+  .on("data", function async (chunk: string) {
+    data = chunk
+      .split(" ")
+      .join()
+      .replace(/[\r\n]/gm, "")
+      .split(",");
+    // Perform chunk logic
+    console.log(data.length);// Get a good approximation of string array to be sent to server
+    console.log((JSON.stringify(data).length * 8)*0.000001) // Get a good approximation of string array size in Mb's to be sent to server
+    // For each chunk, send an update to mongoDB
+    // reset data
+    data = [""];
+    loop++;
+  })
+  .on("end", function () {
+    console.log("###################");
+    console.log(data);
+    console.log('loopCount: ', loop);
+    // here you see all data processed at end of file
+  });
