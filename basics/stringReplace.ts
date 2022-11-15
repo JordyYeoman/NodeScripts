9e2f7ff28ae4710fccc34205b79c30e1fdccc65f
Jordy Yeoman <jordyyeoman@gmail.com>
2022-11-15 12:03:13 +0800

    More basic string manipulation

--- basics/stringReplace.ts
+++ basics/stringReplace.ts
@@ -4,3 +4,13 @@ let newStr2 = str.replace(/\|/g, "\n - ");
 
 console.log("newStr", newStr);
 console.log("newStr2", newStr2);
+
+const testStr = "h e l l o";
+const demoStr = testStr.replace(" ", ""); // Does not work, returns original string
+const demoStr2 = testStr.replace(/\s/g, "");
+console.log("demoStr: ", demoStr);
+console.log("demoStr2:", demoStr2);
+
+let testStr2 = "  Hello World ! ";
+const demoStr3 = testStr2.replace(/\s/g, "");
+console.log("demoStr3:", demoStr3);
