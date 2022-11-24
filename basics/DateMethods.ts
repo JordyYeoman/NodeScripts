b59c4f451321978eb7ce88a1ee5eabb616d02830
Jordy Yeoman <jordyyeoman@gmail.com>
2022-11-24 09:27:56 +0800

    Date mthodz

--- basics/DateMethods.ts
+++ basics/DateMethods.ts
@@ -38,4 +38,24 @@ function capitalizeFirstLetter(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
+function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 console.log(capitalizeFirstLetter("foo")); // Foo
+
+for (let i = 0; i <= 6; i++) {
+  // Generate 3 different date sets in milliseconds
+  const dateBoi = new Date("2024-09-18");
+  const noLongerShowingPromo = dateBoi.getTime() + getRandomInt(244000, 244900);
+  const promoShowingButExpired =
+    dateBoi.getTime() + getRandomInt(245000, 250000);
+  const promoAvailble = dateBoi.getTime() + getRandomInt(243000, 243900);
+
+  // console.log("Promo Displaying from: ", promoAvailble);
+  // console.log("Promo Greyed Out: ", promoShowingButExpired);
+  // console.log("Promo No Longer Visible (Dropped): ", noLongerShowingPromo);
+  console.log(promoAvailble, promoShowingButExpired, noLongerShowingPromo);
+}
