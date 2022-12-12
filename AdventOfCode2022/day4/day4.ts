2967636c8e4e501a4e1f86e67cd4d3ea6f7474e0
Jordy Yeoman <jordyyeoman@gmail.com>
2022-12-12 22:39:40 +0800

    Wrong answers only kappa

--- AdventOfCode2022/day4/day4.ts
+++ AdventOfCode2022/day4/day4.ts
@@ -2,4 +2,43 @@ import fs from "fs";
 import readline from "readline";
 
 // --- Day 4: Camp Cleanup ---
-// Find pairs where one range fully contain the other?
+
+const filename = 'input.txt';
+let pairsContained = 0;
+let data: string[] = fs.readFileSync(filename, 'utf8').split('\n');
+
+const getSideWithHighestNumber = (l: string[], r: string[]): string => {
+    let lNum = parseInt(l[1]); 
+    let rNum = parseInt(r[1]);
+    if(lNum === rNum) {
+        return '';
+    };
+    if(lNum >rNum) {
+        return 'LEFT';
+    }
+    return 'RIGHT';
+};
+
+data.map((pair: string)=> {
+    // Get Left and right sides
+    let leftSide = pair.split(',')[0].split('-');
+    let rightSide = pair.split(',')[1].split('-');
+
+    // Find the highest number of the lot - either left[1] or right [1]
+    let side = getSideWithHighestNumber(leftSide, rightSide);
+    
+    if(side === 'LEFT') {
+       if( leftSide[0] < rightSide[0]) {
+        console.log('leftside[0', leftSide[0], rightSide[0])
+        pairsContained += 1;
+       } // Right side fully contained in left side
+    }
+    if(side === 'RIGHT') {
+        if (rightSide[0] < leftSide[0]) {
+            pairsContained += 1;
+        } // Left side fully contained in right side
+    }
+
+})
+
+console.log('Pairs contained: ', pairsContained);
