f47c82aa28bb01b8b525aa491a633e59d192b266
Jordy Yeoman <jordyyeoman@gmail.com>
2022-12-20 16:22:37 +0800

    chore: add parsing config

--- HeatmapDataProcessing/index.ts
+++ HeatmapDataProcessing/index.ts
@@ -9,6 +9,12 @@ let data2: string[][] = data.map(line => line.split(','))
 // We only need the values at indexs < 11, others can be removed
 let data3: string[][] = data2.map(line => line.slice(0, 11));
 
+// Process table lines for simpler use in app, we want all lowercase, no funky chars and no spaces in column names
+let columnHeaders: string[] = data3.slice(0, 1)[0].map((char) => char.trim().replace(/^a-zA-Z0-9 ]/g, '').replace(' ', '').replace('°', '').toLowerCase());
+
+// Set new headers
+data3[0] = columnHeaders;
+
 // Write output to CSV file
 const writeStream = fs.createWriteStream(`PROCESSED - ${filename}`);
 data3.forEach(line => writeStream.write(`${line.join(',')} \n`));
