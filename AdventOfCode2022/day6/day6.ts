ca96848a28499be1e90696b156cf298a84708e90
Jordy Yeoman <jordyyeoman@gmail.com>
2022-12-27 09:44:05 +0800

    chore: Completed answers for part1 and part2

--- AdventOfCode2022/day6/day6.ts
+++ AdventOfCode2022/day6/day6.ts
@@ -1,6 +1,40 @@
 import fs from 'fs';
 
-// --- Day 6: Tuning Trouble ---
+// --- Day 6: Tuning Trouble - 28 mins ---
 let data: string = fs.readFileSync('input.txt', 'utf8');
 
-console.log('data', data);
+// Loop through each characters and check if they are all unique.
+// Find the first 4 characters that are unique
+
+function hasUniqueCharacters(array: string[]) {
+  const set = new Set(array);
+  return set.size === array.length;
+}
+
+// Part One - 27mins - Part Two - 1 min
+function main(input: string) {
+  let arrOfChars = input.split('');
+  let testingChars: string[] = [];
+  let uniqueCodeAtIndex: number = 0;
+
+  // Loop all characters
+  arrOfChars.find((char: string, index: number) => {
+    if (testingChars.length === 14) {
+      console.log('Testing: ', testingChars);
+      // Test for unique
+      if (hasUniqueCharacters(testingChars)) {
+        uniqueCodeAtIndex = index;
+        return true;
+      }
+      // Remove first element
+      testingChars.splice(0, 1);
+    }
+    testingChars.push(char);
+  });
+
+  // Show where first unique value lies
+  console.log(uniqueCodeAtIndex);
+}
+
+// Run algo
+main(data);
