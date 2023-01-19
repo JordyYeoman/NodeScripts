a2efa6a618de4bfd333e5789cd95ddcbd3956757
Jordy Yeoman <jordyyeoman@gmail.com>
2023-01-19 09:35:11 +0800

    chore: further basics

--- basics/filter.ts
+++ basics/filter.ts
@@ -1,12 +1,25 @@
+// Basic example
 const test = [
-    {name: 'jordy', age: 12, group: 'C'},
-    {name: 'tim', age: 2, group: 'QA'},
-    {name: 'bob', age: 5, group: 'DL'}
-]
+  { name: "jordy", age: 12, group: "C" },
+  { name: "tim", age: 2, group: "QA" },
+  { name: "bob", age: 5, group: "DL" },
+];
 
-
-let x = test.filter((person) => person.group === 'C');
-console.log('x',x)
+let x = test.filter((person) => person.group === "C");
+console.log("x", x);
 
 let y = test.filter((person) => person.age < 10);
-console.log('y',y)
+console.log("y", y);
+
+// Nested Array example
+const test2 = [
+  { name: "jordy", age: 12, group: ["C"] },
+  { name: "tim", age: 2, group: ["QA"] },
+  { name: "bob", age: 5, group: ["DL", "MG"] },
+];
+
+let z = test2.filter((person) => person.group.includes("C"));
+console.log("z", z);
+
+let k = test2.filter((person) => person.group.includes("DL"));
+console.log("k", k);
