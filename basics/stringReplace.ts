8b96ba2aa2bee790d6573e94df34ca4ff3a0d803
Jordy Yeoman <jordyyeoman@gmail.com>
2023-02-14 10:42:41 +0800

    chore: add more string filter testing

--- basics/stringReplace.ts
+++ basics/stringReplace.ts
@@ -26,8 +26,21 @@ let testStr6 =
   "Hello World! | Hello Muppet! Hello World! | Hello Muppet! Hello World! | Hello Muppet! Hello World! | Hello | Muppet! Hello World! || Hello Mup|pet!";
 console.log("Splitting weird string: ", testStr6.replace(/\s/g, "").split("|"));
 
-let testStr7 = "Woodbine Mohawk Park"
-console.log(testStr7.replace(' ', '-'));
+let testStr7 = "Woodbine Mohawk Park";
+console.log(testStr7.replace(" ", "-"));
 
 let testStr8 = "Woodbine Mohawk Park";
-console.log(testStr8.replace(/ /g, '-'));
+console.log(testStr8.replace(/ /g, "-"));
+
+const testStr9 =
+  "- The maximum Bonus Cash for this promotion is $50. - This offer is not claimable through Mates Mode pot bets. - Bets placed using Odds Boost are eligible for this offer.";
+const updates: string = testStr9.replace(/\b\s-\s\b/g, "\n- ");
+const updates2: string = testStr9.split(" - ").join("\n- ");
+console.log("updates: ", updates);
+console.log("updates2: ", updates2);
+
+const testStr10 = "misc/hello-world";
+if (testStr10.includes("hel")) {
+  console.log("testStr10: INCLUDES pass");
+}
+console.log("testStr10: ", testStr10.replace("/", "-"));
