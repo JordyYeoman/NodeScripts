85576515cc59ee92ecc68bb813ce7651b5dc8f8a
Jordy Yeoman <jordyyeoman@gmail.com>
2023-02-22 17:51:59 +0800

    chore: maps

---
+++ basics/mappyBois.ts
@@ -0,0 +1,9 @@
+let myMap = new Map<string, string>([
+  ["3ed7f69b-a717-4529-ae3e-f396b23cecb0", "FULL"],
+  ["169f0b4d-2ef1-4eb0-a3fe-91f50102d786", "SEND"],
+  ["81192825-1dd0-4f01-849d-2f1e2bdc2536", "SHAZAM"],
+]);
+
+let x = myMap.get("3ed7f69b-a717-4529-ae3e-f396b23cecb0");
+
+console.log("x", x);
