b094d7bdf3ae29fc8ffab0647c8e561d878f668a
Jordy Yeoman <jordyyeoman@gmail.com>
2023-04-20 08:30:31 +0800

    feat: comparison testing

---
+++ basics/conditionsBra.ts
@@ -0,0 +1,34 @@
+let x1 = true;
+let y2 = false;
+let z3 = false;
+
+if (!x1 || y2 || z3) {
+    console.log('Whatttt you silly')
+}
+
+let testObj = {
+    tools: {
+        hammer: {
+            dualWield: [
+                'axe'
+            ],
+            strength: 2,
+            durability: 1
+        },
+        axe: {
+            dualWield: [
+                'hammer'
+            ],
+            strength: 3,
+            durability: 2
+        }
+    }
+}
+
+if(testObj?.tools?.hammer?.dualWield) {
+    console.log('Hammer is good!');
+}
+
+if(testObj?.tools?.axe?.dualWield?.length > 0) {
+    console.log('Axe is good!')
+}
