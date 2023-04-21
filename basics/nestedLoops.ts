b8b523cb47b777c97c5bf17da4569fecb0358fd3
Jordy Yeoman <jordyyeoman@gmail.com>
2023-04-21 08:50:59 +0800

    chore: testing basic loop patterns

---
+++ basics/nestedLoops.ts
@@ -0,0 +1,10 @@
+const loopCount = 100;
+let loopTimes = 0;
+
+for(let i = 0; i < loopCount; i++) {
+    for(let j = 0; j < loopCount; j++) {
+        console.log('----------')
+        console.log('Loop number: ', loopTimes += 1);
+        console.log('----------')
+    }
+}
