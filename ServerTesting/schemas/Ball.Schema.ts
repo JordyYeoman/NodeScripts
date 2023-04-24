774016a45b19df5e84fdb5238a067a3c6efac877
Jordy Yeoman <jordyyeoman@gmail.com>
2023-04-24 22:52:33 +0800

    feat: continue game loop

--- ServerTesting/schemas/Ball.Schema.ts
+++ ServerTesting/schemas/Ball.Schema.ts
@@ -3,12 +3,16 @@ export class Ball {
   height: number;
   velocity: number;
   friction: number;
+  x: number;
+  y: number;
 
   constructor(width: number, height: number) {
     this.width = width;
     this.height = height;
     this.velocity = 0;
     this.friction = 9.8;
+    this.x = 0;
+    this.y = 0;
   }
 
   update() {
