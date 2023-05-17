ccabed7c7fff74e2577e95e0a9d84d2055d7e7e7
Jordy Yeoman <jordyyeoman@gmail.com>
2023-05-17 20:15:41 +0800

    chore: update auth req

--- ScraperV3/login.ts
+++ ScraperV3/login.ts
@@ -1,19 +1,65 @@
-import { authenticate } from "betfair-api-ts";
+import { authenticate, listEventTypes } from "betfair-api-ts";
 import fs from "fs";
 
 export const loginBetfair = async () => {
-  const key = "";
-  const cert = "";
-  if (!key || !cert) return;
+  const {
+    BETFAIR_USERNAME,
+    BETFAIR_PASSWORD,
+    BETFAIR_APP_KEY,
+    BETFAIR_CERT,
+    BETFAIR_CERT_KEY,
+    BETFAIR_CHALLENGE,
+  } = process.env;
+
+  if (
+    !BETFAIR_USERNAME ||
+    !BETFAIR_PASSWORD ||
+    !BETFAIR_APP_KEY ||
+    !BETFAIR_CERT ||
+    !BETFAIR_CERT_KEY ||
+    !BETFAIR_CHALLENGE
+  )
+    return;
+
+  console.log(
+    "FOOKIN SEND IT: ",
+    BETFAIR_USERNAME,
+    BETFAIR_PASSWORD,
+    BETFAIR_APP_KEY,
+    BETFAIR_CERT,
+    BETFAIR_CERT_KEY,
+    BETFAIR_CHALLENGE
+  );
 
   let x = await authenticate({
-    username: process.env.BETFAIR_USERNAME ?? "",
-    password: process.env.BETFAIR_PASSWORD ?? "",
-    appKey: process.env.BETFAIR_APP_KEY ?? "",
-    certificate: cert, // Self Signed Certificate
-    certificateKey: process.env.BETFAIR_CERT_KEY ?? "", // Self Signed Certificate Key
-    certificatePassword: process.env.BETFAIR_CHALLENGE, // Optional passphrase for Certificate Key
+    username: BETFAIR_USERNAME,
+    password: BETFAIR_PASSWORD,
+    appKey: BETFAIR_APP_KEY,
+    certificate: BETFAIR_CERT, // Self Signed Certificate
+    certificateKey: BETFAIR_CERT_KEY, // Self Signed Certificate Key
+    certificatePassword: BETFAIR_CHALLENGE, // Optional passphrase for Certificate Key
   });
+
+  //   listEventTypes();
 
   console.log("x", x);
 };
+
+// export declare type MarketFilter = {
+//     bspOnly?: boolean;
+//     competitionIds?: string[];
+//     eventIds?: string[];
+//     eventTypeIds?: string[];
+//     exchangeIds?: string[];
+//     inPlayOnly?: boolean;
+//     marketBettingTypes?: MarketBettingType[];
+//     marketCountries?: string[];
+//     marketIds?: string[];
+//     marketStartTime?: TimeRange;
+//     marketTypeCodes?: string[];
+//     raceTypes?: string[];
+//     textQuery?: string;
+//     turnInPlayEnabled?: boolean;
+//     venues?: string[];
+//     withOrders?: OrderStatus[];
+// };
