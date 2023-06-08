b9493885aacd2c38e296a25f0783b612c203f89f
Jordy Yeoman <jordyyeoman@gmail.com>
2023-06-08 23:00:33 +0800

    chore: understanding linked lists

--- primeagen_algos_course/dataStructures/linkedList.ts
+++ primeagen_algos_course/dataStructures/linkedList.ts
@@ -1,6 +1,7 @@
-// Is a = [] an array? 
+// Is a = [] an array?
 // How come we can push into the array? AKA - resizable...
 
 // ======= LINKED LIST ===========
 // Inside of a linked list, insertions and deletions are constant time O(1), since those operations are all constant.
 
+// OBJ A { data: {} , prev: node, next: node } -> // OBJ B { data: {} , prev: node, next: node }..... etc etc
