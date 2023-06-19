f18c19dedf72f1945e5843c2385e70e14838383d
Jordy Yeoman <jordyyeoman@gmail.com>
2023-06-19 12:26:51 +0800

    chore: understanding basic promises again...

---
+++ basics/promises.ts
@@ -0,0 +1,27 @@
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 1000, "foo");
+});
+const promise4 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 1000, "foo");
+});
+
+const handlePromises = async (): Promise<any> => {
+  const res = await Promise.all([promise1, promise2, promise3, promise4])
+    .then((values) => {
+      return values;
+    })
+    .catch(() => {
+      return null;
+    });
+
+  return res;
+};
+
+const doStuff = async () => {
+  let handledBra = await handlePromises();
+  console.log("handledBra", handledBra);
+};
+
+doStuff();
