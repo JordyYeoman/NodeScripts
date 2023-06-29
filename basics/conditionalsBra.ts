347b87a1a89a885e509d309af522d06a919cbb58
Jordy Yeoman <jordyyeoman@gmail.com>
2023-06-29 22:15:26 +0800

    chore: filtering exercises

--- basics/conditionalsBra.ts
+++ basics/conditionalsBra.ts
@@ -11,3 +11,9 @@ console.log('filteredThingo', filteredThingo);
 if(filteredThingo) {
     console.log('Yeah dude still filtered!')
 }
+
+const z = [1, 'h', 4, 'z', {}];
+
+console.log('z filtered: ', z.filter((k) => typeof k === 'object'));
+console.log('z filtered: ', z.filter((k) => typeof k === 'string'));
+console.log('z filtered: ', z.filter((k) => typeof k === 'number'));
