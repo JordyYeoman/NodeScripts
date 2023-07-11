da8edb232b2acd31c6f546cdaf2885a3a07fc7cd
Jordy Yeoman <jordyyeoman@gmail.com>
2023-07-12 07:23:50 +0800

    chore: testing simple foor loop offset

---
+++ basics/simpleLoop.ts
@@ -0,0 +1,12 @@
+
+const raceIds = new Array(200).fill('hi');
+const promises: any[] = [];
+
+for (let i = 0; i < raceIds.length; i += 19) {
+    const batch = raceIds.slice(i, i + 19);
+
+    console.log('batch: ', batch);
+    promises.push({ race_ids: batch });
+}
+
+console.log('promises: ', promises);
