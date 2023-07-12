a303b242b2d67aa26e5633ffe5e0d368e9d3bf0c
Jordy Yeoman <jordyyeoman@gmail.com>
2023-07-12 17:13:37 +0800

    chore: simple loop

--- basics/simpleLoop.ts
+++ basics/simpleLoop.ts
@@ -3,6 +3,7 @@ const raceIds = new Array(200).fill('hi');
 const promises: any[] = [];
 
 for (let i = 0; i < raceIds.length; i += 19) {
+    console.log('i', i);
     const batch = raceIds.slice(i, i + 19);
 
     console.log('batch: ', batch);
