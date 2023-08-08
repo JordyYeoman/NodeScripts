a653f66a317c4ebf86d2a8cfbd71d1293b1d614d
Jordy Yeoman <jordyyeoman@gmail.com>
2023-08-08 19:57:38 +0800

    feat: more promise looping

--- basics/promiseLoop.ts
+++ basics/promiseLoop.ts
@@ -24,4 +24,19 @@ const doMagic = async () => {
   }
 };
 
-doMagic();
+const morePromises = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Sent it!");
+    }, 2000);
+  });
+
+const doMoreMagic = async () => {
+  for (let i = 0; i < data.length; i++) {
+    let x = await morePromises();
+    console.log("x", x);
+  }
+};
+
+// doMagic();
+doMoreMagic();
