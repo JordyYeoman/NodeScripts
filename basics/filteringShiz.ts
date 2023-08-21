e464d17523130c48355a195803849aedf296acdf
Jordy Yeoman <jordyyeoman@gmail.com>
2023-08-21 14:31:51 +0800

    feat: testing simple arr methods again

---
+++ basics/filteringShiz.ts
@@ -0,0 +1,19 @@
+const filterMeDaddy = (x: any[]) => {
+  return x.filter((x) => x);
+};
+
+const doSomethingBra = (x: any[]) => {
+  az.forEach((z: any) => {
+    console.log(z);
+    if (z === 0) {
+      return;
+    }
+    console.log("Loop still running");
+  });
+};
+
+const az = ["0", 0, undefined, null, "hello"];
+
+console.log("filter: ", filterMeDaddy(az));
+
+console.log("forEach", doSomethingBra(az));
