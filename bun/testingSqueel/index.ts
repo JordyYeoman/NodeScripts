fb86b3260bb0a6cbabcdf656206d14a7fa8bee75
Jordy Yeoman <jordyyeoman@gmail.com>
2023-09-10 15:14:59 +0800

    feat: futher sqllite driver investigation with bun runtime

--- bun/testingSqueel/index.ts
+++ bun/testingSqueel/index.ts
@@ -9,7 +9,21 @@ const inMemoryDB = new Database(":memory:");
 const query = db.query("SELECT * FROM tbl1;");
 let x = query.get(); // => { message: "Hello world" }
 
-console.log(x);
+// Transactions
+const insertData = db.prepare(
+  "INSERT INTO tbl1 (one, two, three) VALUES ($one, $two, $three)"
+); // prepare a transaction in db cache
+const insertDataPayload = db.transaction((d) => {
+  for (const data of d) insertData.run(data);
+}); // this is all preparation for db insertion
+
+insertDataPayload([
+  { $one: "Keanu", $two: 10, $three: 4122 },
+  { $one: "Salem", $two: 102, $three: 1122 },
+  { $one: "Crookshanks", $two: 109, $three: 1512 },
+]);
+
+console.log(`Inserted payload: ${insertDataPayload} payload into tbl1`);
 
 // Disconnect from DB
 db.close();
