6bf7140015d1b2a7ec3c7799606c275cd6d5f3d0
Jordy Yeoman <jordyyeoman@gmail.com>
2023-09-20 08:31:46 +0800

    feat: testing basic ternary logic

--- basics/logTesting.ts
+++ basics/logTesting.ts
@@ -1,5 +1,11 @@
 const IS_DEV = true;
-const id = 'AIUB8124BAF';
-const screenName = 'HomeScreen';
+const id = "AIUB8124BAF";
+const screenName = "HomeScreen";
 
-!IS_DEV || console.log(id, 'refocussed', screenName);
+!IS_DEV || console.log(id, "refocussed", screenName);
+
+// Verify ternary logic
+let xT = true;
+let yF = "Running secondary logic";
+
+console.log("test: ", xT ?? yF);
