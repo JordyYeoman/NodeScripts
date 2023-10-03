483b5989523c657dd08266a3e76d5b91cd1de8f7
Jordy Yeoman <jordyyeoman@gmail.com>
2023-10-03 22:00:05 +0800

    feat: more type testing

--- basics/type-aliases.ts
+++ basics/type-aliases.ts
@@ -8,14 +8,19 @@ type DogLover = {
     hasDog: boolean,
 }
 
+type BigDog = {
+    horseSize: boolean,
+}
+
 // Combination of types
-type PersonWhoLovesDogs = Person & DogLover;
+type PersonWhoLovesDogs = Person & DogLover & BigDog;
 
 const jordy = {
     name: 'Jordy',
     age: 28,
     lovesDogs: true,
     hasDog: true,
+    horseSize: true,
 } satisfies PersonWhoLovesDogs;
 
 const x = [1, 2, 3];
