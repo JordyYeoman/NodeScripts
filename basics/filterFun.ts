93fbcb64eafbd5260584c9b42bba953889f82230
Jordy Yeoman <jordyyeoman@gmail.com>
2023-10-09 11:11:29 +0800

    feat: testing filter args

--- basics/filterFun.ts
+++ basics/filterFun.ts
@@ -2,3 +2,8 @@ const randoStuff = [20, 4000, 1251, 201, 9999, 5121];
 
 const filteredShiz = randoStuff.filter((r) => r === 20);
 console.log("filtered: ", filteredShiz);
+
+const filterStuff = (arr: number[], alt?: number) => arr.filter((r) => (alt === undefined || r < alt));
+
+console.log('[FILTER] filterStuff: ', filterStuff(randoStuff))
+console.log('[FILTER] filterStuff: ', filterStuff(randoStuff, 4001))
