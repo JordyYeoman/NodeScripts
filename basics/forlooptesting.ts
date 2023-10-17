b3561e8dd16f3698bcb0ec2eaedd56ce46f40c9d
Jordy Yeoman <jordyyeoman@gmail.com>
2023-10-17 14:38:58 +0800

    feat: forloop sync nature

---
+++ basics/forlooptesting.ts
@@ -0,0 +1,19 @@
+const myPromise = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(function () {
+      resolve("Count");
+    }, 1000);
+  });
+
+const waitNVueRenderCycles = async (numOfTicksToWait: number) => {
+  for(let i = 0; i < numOfTicksToWait; i++) {
+    await myPromise();
+  }
+}
+
+const doStuffSynchronously = async () => {
+    await waitNVueRenderCycles(3);
+    console.log('All done!');
+}
+
+doStuffSynchronously();
