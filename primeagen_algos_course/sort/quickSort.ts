17fa7b88c2ac392f80a407bd5c0fb4ef43297b63
Jordy Yeoman <jordyyeoman@gmail.com>
2023-10-21 09:59:06 +0800

    feat: quicksort walkthrough

--- primeagen_algos_course/sort/quickSort.ts
+++ primeagen_algos_course/sort/quickSort.ts
@@ -1,2 +1,41 @@
+function qs(arr: number[], lo: number, hi: number): void {
+  // When lo and hi meet, bad time :D
+  if (lo >= hi) {
+    // We have finished sorting array
+    return;
+  }
+
+  const pivotIdx = partition(arr, lo, hi);
+
+  qs(arr, lo, pivotIdx - 1);
+  qs(arr, pivotIdx + 1, hi);
+}
+
+function partition(arr: number[], lo: number, hi: number): number {
+  const pivot = arr[hi];
+
+  let idx = lo - 1;
+
+  // Loop from start of sub array to end of sub array.
+  for (let i = lo; i < hi; i++) {
+    // While number at current arr position is less or equal to pivot, it is still sorted
+    if (arr[i] <= pivot) {
+      idx++;
+      const tmp = arr[i];
+      // Swap the pivot
+      arr[i] = arr[idx];
+      arr[idx] = tmp;
+    }
+  }
+
+  idx++;
+  arr[hi] = arr[idx];
+  arr[idx] = pivot;
+
+  return idx;
+}
+
 // Sort in place, so original arr will be modified
-export const quickSort = (arr: number[]): void => {};
+export function quickSort(arr: number[]): void {
+  qs(arr, 0, arr.length - 1);
+}
