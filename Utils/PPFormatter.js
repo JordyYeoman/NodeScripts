aeda6755382eec86e9cd45ca466c49b060ed09ad
Jordy Yeoman <jordyyeoman@gmail.com>
2023-10-24 07:04:46 +0800

    feat more utils

--- Utils/PPFormatter.js
+++
@@ -1,6 +0,0 @@
-// Valid input for Salesforce dashboard only expects a format like below.
-// Many other iterations have been tried with no avail, please run `node PPFormatter.js` to format your event/race ids into the expected format.
-// Paste the result into the SF InteractionStudio dashboard :)
-
-// Example attribute input
-// [{id:"123wooo"},{id: "123wooo111"},{ id: "123wooo" },{ id: "123wooo111" },{ id: "123wooo" },{ id: "123wooo111" }];
---
+++ Utils/power.ts
@@ -0,0 +1,31 @@
+const fs = require("fs");
+
+function getRandomNumber(
+  lo: number,
+  hi: number,
+  multiplier: number = 1
+): string {
+  return ((Math.random() * (1 + hi - lo) + lo) * multiplier).toFixed(3);
+}
+
+function getListOfConsumption() {
+  const arr: string[] = [];
+
+  for (let i = 0; i < 365; i++) {
+    arr.push(getRandomNumber(0, 0.789));
+  }
+
+  return arr;
+}
+
+const d = getListOfConsumption().join("\n");
+
+fs.writeFile("power.csv", d, "utf8", function (err: unknown) {
+  if (err) {
+    console.log(
+      "Some error occured - file either not saved or corrupted file saved."
+    );
+  } else {
+    console.log("It's saved!");
+  }
+});
