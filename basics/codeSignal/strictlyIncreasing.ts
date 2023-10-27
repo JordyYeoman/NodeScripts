cc08da3a25bb308368358ebd821497e43a7ae306
Jordy Yeoman <jordyyeoman@gmail.com>
2023-10-27 10:34:58 +0800

    fix: remove comments

--- basics/codeSignal/strictlyIncreasing.ts
+++ basics/codeSignal/strictlyIncreasing.ts
@@ -1,22 +1,3 @@
-// def almostIncreasingSequence(sequence):
-//     removed = 0
-//     previous_maximum = maximum = float('-infinity')
-//     for s in sequence:
-//         if s > maximum:
-//             # All good
-//             previous_maximum = maximum
-//             maximum = s
-//         elif s > previous_maximum:
-//             # Violation - remove current maximum outlier
-//             removed += 1
-//             maximum = s
-//         else:
-//             # Violation - remove current item outlier
-//             removed += 1
-//         if removed > 1:
-//             return False
-//     return True
-
 function strictlyIncreasing(sequence: number[]): boolean {
   let removed = 0;
   let previousMaximum = Number.NEGATIVE_INFINITY;
