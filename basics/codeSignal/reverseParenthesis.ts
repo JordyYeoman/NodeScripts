5fcb1e18077732de0b24fce63fce10b14c39603e
Jordy Yeoman <jordyyeoman@gmail.com>
2023-11-04 11:45:26 +0800

    feat: first phase of reverse

---
+++ basics/codeSignal/reverseParenthesis.ts
@@ -0,0 +1,39 @@
+function reverseParenthesis(inputString: string): string {
+  const arr = Array.from(inputString);
+  let pos1 = 0;
+  let pos2 = 0;
+
+  while (pos1 < arr.length) {
+    if (arr[pos1] === "(") {
+      pos2 = pos1 + 1;
+      const stack: string[] = [];
+      let openParentheses = 1; // To handle nested parentheses
+
+      while (pos2 < arr.length && openParentheses > 0) {
+        if (arr[pos2] === "(") {
+          openParentheses++;
+        } else if (arr[pos2] === ")") {
+          openParentheses--;
+        }
+
+        stack.push(arr[pos2]);
+        pos2++;
+      }
+
+      // Reverse the characters within parentheses
+      const reversed = stack.reverse();
+
+      // Replace the original substring with the reversed one
+      arr.splice(pos1, pos2 - pos1, ...reversed);
+    }
+
+    pos1++;
+  }
+
+  return arr.filter((char) => char !== ")").join("");
+}
+
+console.log("rab", solution("(bar)"));
+console.log("arabz", solution("a(bar)z"));
+console.log("foorabbaz", solution("foo(bar)baz"));
+console.log("foorabbazmilb", solution("foo(bar)baz(blim)"));
