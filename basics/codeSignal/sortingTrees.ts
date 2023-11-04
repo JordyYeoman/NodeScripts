dfa340a3fadbafc6ed75788a528b96b67bc63ee4
Jordy Yeoman <jordyyeoman@gmail.com>
2023-11-04 09:33:31 +0800

    feat tree sorting

--- basics/codeSignal/sortingTrees.ts
+++ basics/codeSignal/sortingTrees.ts
@@ -1,25 +1,19 @@
-// Tree is -1
-//
-
 function sortedTrees(a: number[]): number[] {
   const treePositions: number[] = [];
 
+  // Iterate over the input array to collect tree positions
   a.forEach((element: number, index) => {
     if (element === -1) {
-      // Remember the position of the tree
       treePositions.push(index);
     }
   });
 
-  // Loop again to remove the trees from original array
-  // So we can sort
-  a.forEach((element, index) => (element === -1 ? a.splice(index, 1) : null));
+  // Filter out the -1 elements and sort the remaining elements
+  const sortedArray = a
+    .filter((element) => element !== -1)
+    .sort((a, b) => a - b);
 
-  const sortedArray = a.sort();
-
-  console.log("sortedArray", sortedArray);
-
-  // Add the trees back in
+  // Reinsert the -1 'trees' at their original positions
   treePositions.forEach((tree: number) => {
     sortedArray.splice(tree, 0, -1);
   });
@@ -27,19 +21,19 @@ function sortedTrees(a: number[]): number[] {
   return sortedArray;
 }
 
-// console.log("[1 ,2, -1, 3]", sortedTrees([1, 3, -1, 2]));
+console.log("[1 ,2, -1, 3]", sortedTrees([1, 3, -1, 2]));
 console.log(
-  "A: [-1, 150, 190, 170, -1, -1, 160, 180]",
+  "A: [-1, 150, 160, 170, -1, -1, 180, 190]",
   sortedTrees([-1, 150, 190, 170, -1, -1, 160, 180])
 );
-// console.log("A: [-1, -1, -1, -1, -1]", sortedTrees([-1, -1, -1, -1, -1]));
-// console.log("A:  [-1]", sortedTrees([-1]));
-// console.log("A: [4, 2, 9, 11, 2, 16]", sortedTrees([2, 2, 4, 9, 11, 16]));
-// console.log(
-//   "A: [2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1]",
-//   sortedTrees([1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 2])
-// );
-// console.log(
-//   "A: [23, 54, -1, 43, 1, -1, -1, 77, -1, -1, -1, 3]",
-//   sortedTrees([1, 3, -1, 23, 43, -1, -1, 54, -1, -1, -1, 77])
-// );
+console.log("A: [-1, -1, -1, -1, -1]", sortedTrees([-1, -1, -1, -1, -1]));
+console.log("A:  [-1]", sortedTrees([-1]));
+console.log("A: [2, 2, 4, 9, 11, 16]", sortedTrees([4, 2, 9, 11, 2, 16]));
+console.log(
+  "A: [2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1]",
+  sortedTrees([1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 2])
+);
+console.log(
+  "A: [1, 3, -1, 23, 43, -1, -1, 54, -1, -1, -1, 77]",
+  sortedTrees([1, 3, -1, 23, 43, -1, -1, 54, -1, -1, -1, 77])
+);
