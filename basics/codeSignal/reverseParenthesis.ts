4704f05392ed893fdc915efd56ca3f3b223e74ce
Jordy Yeoman <jordyyeoman@gmail.com>
2023-11-10 21:19:31 +0800

    feat: fixing reverse parenthesis

--- basics/codeSignal/reverseParenthesis.ts
+++ basics/codeSignal/reverseParenthesis.ts
@@ -1,13 +1,12 @@
 function reverseParenthesis(inputString: string): string {
   const arr = Array.from(inputString);
   let pos1 = 0;
-  let pos2 = 0;
 
   while (pos1 < arr.length) {
     if (arr[pos1] === "(") {
-      pos2 = pos1 + 1;
+      let pos2 = pos1 + 1;
       const stack: string[] = [];
-      let openParentheses = 1; // To handle nested parentheses
+      let openParentheses = 1;
 
       while (pos2 < arr.length && openParentheses > 0) {
         if (arr[pos2] === "(") {
@@ -16,14 +15,14 @@ function reverseParenthesis(inputString: string): string {
           openParentheses--;
         }
 
-        stack.push(arr[pos2]);
+        if (openParentheses > 0) {
+          stack.push(arr[pos2]);
+        }
+
         pos2++;
       }
 
-      // Reverse the characters within parentheses
       const reversed = stack.reverse();
-
-      // Replace the original substring with the reversed one
       arr.splice(pos1, pos2 - pos1, ...reversed);
     }
 
@@ -37,5 +36,3 @@ console.log("rab", reverseParenthesis("(bar)"));
 console.log("arabz", reverseParenthesis("a(bar)z"));
 console.log("foorabbaz", reverseParenthesis("foo(bar)baz"));
 console.log("foorabbazmilb", reverseParenthesis("foo(bar)baz(blim)"));
-
-// TODO: Solve last test case.
