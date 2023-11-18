db76975ae078e8b3a54636366dbbd4223b969d0f
Jordy Yeoman <jordyyeoman@gmail.com>
2023-11-18 11:24:14 +0800

    feat: testin flatten

--- basics/flattenObj.ts
+++ basics/flattenObj.ts
@@ -1,8 +1,14 @@
-const h = [{ hello: "world" }, { sup: "homie" }];
+const h = [
+  [{ testing: "master" }],
+  [{ testing: "master" }],
+  [{ testing: "master" }],
+];
 
+// Essentially bring all the nested array items up one level into the parent array.
 const z = h.flatMap((styles) => styles);
 const k = h.reduce(
   (accumulator, currentValue) => ({ ...accumulator, ...currentValue }),
   {}
 );
 console.log("k", k);
+console.log("z", z);
