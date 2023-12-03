a85f32f4666d9722a3a87755b74838a36a014647
Jordy Yeoman <jordyyeoman@gmail.com>
2023-12-03 11:41:58 +0800

    feat: full send

--- AdventOfCode2023/day1/day1.ts
+++ AdventOfCode2023/day1/day1.ts
@@ -1,33 +1,90 @@
+// // Part One
+// import { readFileSync } from 'fs';
+
+// const file = readFileSync('day1.txt', 'utf-8');
+
+// let total = 0;
+
+// const getNumberFromString = (line: string) => {
+//   // Find first number in the string
+//   for (const char of line) {
+//     const parsedNumber = parseInt(char);
+
+//     if (!isNaN(parsedNumber)) {
+//       return char;
+//     }
+//   }
+// };
+
+// file.split('\n').forEach((line) => {
+//   let chars = '';
+
+//   // Once we have broken the intial loop, we want to reverse the incoming string
+//   // and once again find the first number.
+//   chars += getNumberFromString(line);
+
+//   // Find the second number, order from the back of string
+//   const reversedLine = line.split('').reverse().join(' ');
+//   chars += getNumberFromString(reversedLine);
+
+//   // Join them, then add to total
+//   total += parseInt(chars);
+// });
+
+// console.log('total: ', total);
+// Part Two
+
 import { readFileSync } from 'fs';
 
 const file = readFileSync('day1.txt', 'utf-8');
 
 let total = 0;
 
-const getNumberFromString = (line: string) => {
-  // Find first number in the string
-  for (const char of line) {
-    const parsedNumber = parseInt(char);
+// Map of number words
+const nums = new Map([
+  ['one', 1],
+  ['two', 2],
+  ['three', 3],
+  ['four', 4],
+  ['five', 5],
+  ['six', 6],
+  ['seven', 7],
+  ['eight', 8],
+  ['nine', 9],
+]);
 
+const getNumberFromString = (characters: any) => {
+  let currentWord = '';
+  // Find first number or word
+  for (let char of characters) {
+    currentWord += char;
+
+    const parsedNumber = parseInt(char);
+    // If number, return number
     if (!isNaN(parsedNumber)) {
       return char;
     }
+
+    // Check if word atleast exists in map
+    for (let [key, value] of nums) {
+      if (
+        currentWord.includes(key) ||
+        currentWord.split('').reverse().join('').includes(key)
+      ) {
+        return value.toString();
+      }
+    }
   }
+
+  return currentWord;
 };
 
 file.split('\n').forEach((line) => {
-  let chars = '';
+  const firstNumber = getNumberFromString(line);
+  const reversedLine = line.split('').reverse().join('');
+  const lastNumber = getNumberFromString(reversedLine);
 
-  // Once we have broken the intial loop, we want to reverse the incoming string
-  // and once again find the first number.
-  chars += getNumberFromString(line);
-
-  // Find the second number, order from the back of string
-  const reversedLine = line.split('').reverse().join(' ');
-  chars += getNumberFromString(reversedLine);
-
-  // Join them, then add to total
-  total += parseInt(chars);
+  total += parseInt(firstNumber + lastNumber);
 });
 
 console.log('total: ', total);
