7dfabe7b2ab7dcb0af32999ed16cb258e0a06d7f
Jordy Yeoman <jordyyeoman@gmail.com>
2023-12-04 14:21:46 +0800

    feat: testing map and set

---
+++ basics/set.ts
@@ -0,0 +1,19 @@
+const testArr = [
+  { hello: "world" },
+  { hello: "world" },
+  1,
+  2,
+  3,
+  3,
+  2,
+  1,
+  null,
+  undefined,
+  null,
+  undefined,
+  "helo",
+  "helo",
+];
+
+const s = new Set(testArr);
+console.log("s", s);
