fadcab5a2f00253d2d7b65f49a0ca87c95b8ef8a
Jordy Yeoman <jordyyeoman@gmail.com>
2024-01-04 16:06:03 +0800

    feat: regex cleanup

--- basics/regex.ts
+++ basics/regex.ts
@@ -1,23 +1,5 @@
 // Regex route name matching
-const routes = [
-  "/racing/au-nz",
-  "/racing/domestic",
-  "/racing/international",
-  "/horse-racing",
-  "/horse-racing/au-nz",
-  "/horse-racing/domestic",
-  "/horse-racing/international",
-  "/harness-racing",
-  "/harness-racing/au-nz",
-  "/harness-racing/domestic",
-  "/harness-racing/international",
-  "/greyhound-racing",
-  "/greyhound-racing/au-nz",
-  "/greyhound-racing/domestic",
-  "/greyhound-racing/international",
-  "/sports/blah",
-  "/sports/:id/:name",
-];
+const routes = ["/racing", "/beastmode"];
 
 // Check if any of these contain `racing`
 const pathMatch = "racing";
