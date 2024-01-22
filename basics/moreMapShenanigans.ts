d76c02acdd38545827eff439148d787bfff0d1e7
Jordy Yeoman <jordyyeoman@gmail.com>
2024-01-22 15:27:56 +0800

    feat: more map shenanigans

--- basics/moreMapShenanigans.ts
+++ basics/moreMapShenanigans.ts
@@ -1,4 +1,6 @@
 const map = new Map();
+const m2 = new Map([["ae134", "test"]]);
 
-const x = map.get('nothing');
-console.log('x', x);
+const x = map.get("nothing");
+console.log("x", x);
+console.log("m2 ae134", m2.get("ae134"));
