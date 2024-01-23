9d37645731f7c7c1fad88627e1eca3e62c5d44fe
Jordy Yeoman <jordyyeoman@gmail.com>
2024-01-23 12:42:34 +0800

    feat: map testing

--- basics/moreMapShenanigans.ts
+++ basics/moreMapShenanigans.ts
@@ -1,6 +1,12 @@
 const map = new Map();
 const m2 = new Map([["ae134", "test"]]);
+const m3 = new Map();
+
+m3.set("User", "Jordy");
+m3.set("User2", 123);
 
 const x = map.get("nothing");
 console.log("x", x);
 console.log("m2 ae134", m2.get("ae134"));
+console.log("m3 user", m3.get("User"));
+console.log("m3 user1", m3.get("User2"));
