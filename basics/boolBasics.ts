1bde6ee47d7c2895f967d393f5bef25660ace184
Jordy Yeoman <jordyyeoman@gmail.com>
2024-02-29 13:22:24 +0800

    feat: testing

---
+++ basics/boolBasics.ts
@@ -0,0 +1,10 @@
+const x = true;
+const y = false;
+const z = undefined;
+
+console.log("[DEBUG] x && y", x && y); // false
+console.log("[DEBUG] x && z", x && z); // undefined
+console.log("[DEBUG] !(x && z)", !(x && z)); // true
+console.log("[DEBUG] Boolean(x && z)", Boolean(x && z)); // false
+console.log("[DEBUG] !Boolean(x && z)", !Boolean(x && z)); // true
+console.log("[DEBUG] y && z", y && z); // false
