7dd3093792f8b128ca4ef4736e14bb08a60ed106
Jordy Yeoman <jordyyeoman@gmail.com>
2024-04-15 07:58:07 +0800

    bug: work testing

--- basics/ArrayMethods.ts
+++ basics/ArrayMethods.ts
@@ -13,3 +13,22 @@ const filterOnBool = (condition: boolean, arr: number[]): number[] =>
 console.log("Filter on true: ", filterOnBool(true, testArray));
 console.log("Filter on false: ", filterOnBool(false, testArray));
 console.log("Filter on true: ", filterOnBool(Boolean("True"), testArray));
+
+const w = { eventId: '124jkljb3krl1lh124vas' }
+const t = { eventId: 'lkh1llhvlabdkg;124va1' }
+const weirdArr = [w,t];
+
+// const objTest11 = {
+//   eventId: 'bwe;gkjwpkefw',
+//   id: 'ljgqewgf',
+//   name: 'hello',
+//   number: 4
+// }
+
+// Object.values(objTest11).filter(([, { eventId}]) => {
+//   console.log('[DEBUG] eventId: ', eventId);
+// })
+
+// weirdArr.filter(([, { eventId}]) => {
+//   console.log('[DEBUG] eventId: ', eventId);
+// })
