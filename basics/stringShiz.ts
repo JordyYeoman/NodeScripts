e96796a5da454e513108cc23770c933fd3e52023
Jordy Yeoman <jordyyeoman@gmail.com>
2024-04-26 15:52:14 +0800

    feat: full obj string manipulation testing

--- basics/stringShiz.ts
+++ basics/stringShiz.ts
@@ -12,9 +12,17 @@ console.log('pz: [0]', pz[0])
 
 
 const x = {};
+const k = undefined as any;
 const y = {1: 'hello'};
 
 const z = {
     ...x,
+    ...k,
     ...y,
 }
+const i = { ...z, ...z, ...{ ...z}}
+const j = { ...i, ...z }
+
+console.log('z', z);
+console.log('i', i); // Same memory address of the obj
+console.log('j', j); // Still all same memory address of the object
