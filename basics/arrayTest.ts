1afd4c72eb0e4c2edf3c435491176fd067330e17
Jordy Yeoman <jordyyeoman@gmail.com>
2024-06-07 13:16:50 +0800

    feat: more testing

--- basics/arrayTest.ts
+++ basics/arrayTest.ts
@@ -12,3 +12,10 @@ if (![].length) {
 
 console.log("El[0]", test2[0]?.test);
 console.log('test2', test2[0]);
+
+
+const x = [1];
+console.log('x?.length: ', x?.length);
+console.log('x?.length > 0', x?.length > 0);
+console.log('x?.length truthy?', x?.length ? 'true' : 'false');
+console.log('x?.length > 0 truthy', (x?.length > 0) ? 'true' : 'false');
